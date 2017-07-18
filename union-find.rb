2928813d47553b2188985b188439dd98044e83d8
Dan <dannguyen90@gmail.com>
2017-07-17 22:03:19 -0400

    started creating Set and Integer classes

--- union-find.rb
+++ union-find.rb
@@ -19,12 +19,52 @@
 # and then if you do join(1,3)
 # now 1,2,3,4 are all connected
 
-# (1,3,6)
-# (4,6,8)
-# (4,5,7)
 
-# integers
-# sets
+# each set has a bunch of integers
+# each integer belongs to a set
+# create set class and integer class?
+
+
+@sets_hash = {}
+@int_hash = {}
+
+class Set
+  def initialize(ints_array)
+    @elements = ints_array
+  end
+
+  def elements
+    @elements
+  end
+
+  def elements=(x)
+    @elements = x
+  end
+end
+
+class Integer
+  def initialize(int, set)
+    @int = int
+    @set = set
+  end
+end
+
+class UnionFind
+  def initialize
+    @sets_hash = {}
+    @int_hash = {}
+  end
+
+  def join(x,y)
+    # if x and y are both setless, create a new set with x and y
+    if int_hash[x] == nil && int_hash[y] == nil
+
+    end
+    # if x is setless and y has a set, then add x to y's set
+    # if x has a set and y is setless, then add y to x's set
+    # if x and y have sets, then merge the two sets
+  end
+end
 
 
 class Set
@@ -32,45 +72,3 @@ class Set
   end
 
 end
-
-class UnionFind
-  def initialize
-    @sets_array = [[3,7], [6]]
-  end
-
-  def index_of_set(x)
-    if @sets_array.length == 0
-      return nil
-    end
-    @sets_array.each_with_index do |set, index|
-      set.each do |num|
-        if (num == x)
-          return index
-        end
-      end
-    end
-  end
-
-  def join(x,y)
-    # if x and y are both setless, create a new set with x and y
-    if (index_of_set(x) == nil && index_of_set(y) == nil)
-      @sets_array << [x,y]
-    # if x is setless and y has a set, then add x to y's set
-    elsif (index_of_set(x) == nil)
-      @sets_array[index_of_set(y)] << x
-    # if x has a set and y is setless, then add y to x's set
-    elsif (index_of_set(y) == nil)
-      @sets_array[index_of_set(x)] << y
-    # if x and y have sets, then merge the two sets
-    else
-
-    end
-  end
-  #
-  # def is_connected(1,2)
-  #
-  # end
-end
-
-union1 = UnionFind.new
-puts union1.index_of_set(6)
